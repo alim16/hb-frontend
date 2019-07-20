2405253c5c350c49724647e79349fdb22a245be8
mahamed ali <mohamed.tum@gmail.com>
2019-07-20 07:08:05 +0100

    stateprovider hooked up with mock data

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,25 +1,30 @@
 import React, { useReducer, useEffect, useState } from 'react';
+
 //import './users.css';
 import  {Table}  from "react-bootstrap";
 
 import {getUsers} from "../axApi";
+import { useStateValue } from '../state/state';
+
 
 function Users () {
-    const [users, setUsers] = useState([])
-    const [isLoading, setIsLoading] = useState(false)
+    //const [users, setUsers] = useState([])
+    //const [isLoading, setIsLoading] = useState(false)
+    const [{ theme, users2}, dispatch] = useStateValue();
+    const isLoading = false
 
-    useEffect(()=>{
-      const fetchData = async () => {
-        setIsLoading(true)
-        const res = await getUsers()
-        console.log(res)
-        setUsers(res.data)
-        setIsLoading(false)
-      }
+    // useEffect(()=>{
+    //   const fetchData = async () => {
+    //     setIsLoading(true)
+    //     const res = await getUsers()
+    //     console.log(res)
+    //     setUsers(res.data)
+    //     setIsLoading(false)
+    //   }
 
-      fetchData()
+    //   fetchData()
 
-    },[])
+    // },[])
  
     return (
       <div className="users">
@@ -34,7 +39,8 @@ function Users () {
           </thead>
           {isLoading ? (<p>isLoading...</p>)
           :<tbody>
-            {users.map(u =>
+            {console.log("current theme is", theme)}
+            {users2.map(u =>
                     <tr key={u.id}>
                         <td>{u.first_name}</td>
                         <td>{u.last_name}</td>
--- src/index.js
+++ src/index.js
@@ -8,19 +8,64 @@ import Users from './components/Users'
 import Items from './components/Items'
 import NavBar from './components/NavBar'
 
+import {StateProvider} from './state/state'
 
-const routing = (
-    <Router>
-        <NavBar/>
-      <div>
-        <Route exact path="/" component={App} />
-       <Route path="/users" component={Users} />
-        <Route path="/items" component={Items} /> 
-      </div>
-    </Router>
+ function routing () {
+  const initialState = {
+    theme: { primary: 'green' },
+    "users2": [
+      {
+        "id": 1,
+        "first_name": "Sebastian",
+        "last_name": "Eschweiler",
+        "email": "sebastian@codingthesmartway.com"
+      },
+      {
+        "id": 2,
+        "first_name": "Steve",
+        "last_name": "Palmer",
+        "email": ""
+      },
+      {
+        "id": 3,
+        "first_name": "Ann",
+        "last_name": "Smith",
+        "email": "ann@codingthesmartway.com"
+      }
+    ]
+  };
+  
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'changeTheme':
+        return {
+          ...state,
+          theme: action.newTheme
+        };
+        
+      default:
+        return state;
+    }
+  };
+
+  return (
+  <StateProvider initialState={initialState} reducer={reducer}>
+      <Router>
+          <NavBar/>
+        <div>
+          <Route exact path="/" component={App} />
+        <Route path="/users" component={Users} />
+          <Route path="/items" component={Items} /> 
+        </div>
+      </Router>
+  </StateProvider>
+
   )
 
-ReactDOM.render(routing, document.getElementById('root'));
+}
+
+
+ReactDOM.render(routing(), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
