3c94733ed34a844f5651228a61f1aeacbb5a0292
mahamed ali <mohamed.tum@gmail.com>
2019-07-22 07:08:57 +0100

    added todo list

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-            <h1>Most amazing front page ever</h1>
+            <h1>Most amazing home page ever</h1>
     </div>
   );
 }
